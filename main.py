dc6cf9c9213b479dddd85abc0f92ec1ba5715a8a
wally800 <joshuajw@princeton.edu>
2017-03-02 10:44:35 -0500

    cleaned up code, removed newline characters in input

--- main.py
+++ main.py
@@ -20,24 +20,41 @@ if len(sys.argv) !=2:
 # Open the fits file
 hdulist = fits.open(sys.argv[1])
 
-# Print and get information about the HDU's
+# Print number of HDU's
 print "Number of HDU's: " + str(len(hdulist))
+
+# Define a temporary file to hold information about the hdulist
 hdulist_info_filename = 'hdulist_info.txt'
+
+# Read the hdulist information to the file
 with open(hdulist_info_filename,'w') as output_f:
     hdulist.info(output=output_f)
 
+# Open the file and read in the information
 with open(hdulist_info_filename,'r') as f:
     info = f.readlines()
 
-print "Available HDU's:"
+# Strip off the newline characters at the end
+for i in range(len(info)):
+    info[i] = info[i].strip('\n')
+
+print ""
+print "The HDU's: are"
+print ""
+
+# Get the description of the different columns and print it out
 info_column_description = info[1]
 print info_column_description
+
+# Redefine the info list to be only the relevant information,
+# not any of the extraneous information hdulist.info prints out
 info = info[2:]
 
+# Print out the various HDU's
 for line in info:
     print line
-
-# Get list of which HDU's are image HDU's.
-list_of_image_hdus = [i for i in range(len(info)) if "ImageHDU" in info[i] ]
 print ""
-print "Number of ImageHDU's: " + str(len(list_of_image_hdus))
+
+# Get list of which HDU's are image HDU's, print out their number
+list_of_image_hdus = [i for i in range(len(info)) if "ImageHDU" in info[i] ]
+print "Number of specifically ImageHDU's: " + str(len(list_of_image_hdus))
